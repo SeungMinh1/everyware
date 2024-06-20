9c4c176e42f1d5ca5106b99548c35111a88df957
Ejfq <thdwogus07@naver.com>
2024-06-20 17:52:20 +0900

    0620

---
+++ everyware/src/main/java/com/yedam/app/calendar/mapper/CalendarMapper.java
@@ -0,0 +1,23 @@
+package com.yedam.app.calendar.mapper;
+
+import java.util.List;
+
+import com.yedam.app.calendar.service.CalendarVO;
+
+public interface CalendarMapper {
+	//전체조회
+	public List<CalendarVO> selectCalendarAll();
+	
+	// 단건조회
+	public CalendarVO selectCalendarInfo(CalendarVO calendarVO);
+	
+	// 등록
+	public int insertCalendarInfo(CalendarVO calendarVO);
+	
+	//수정
+	public int updateCalendarInfo(CalendarVO calendarVO);
+	
+	//삭제
+	public int deleteCalendarInfo(CalendarVO calendarVO);
+	
+}
