37f7aded6ce9410d19149264630856fb16120b12
Ejfq <thdwogus07@naver.com>
2024-06-26 09:50:54 +0900

    0626

--- everyware/src/main/java/com/yedam/app/group/controll/GroupController.java
+++ everyware/src/main/java/com/yedam/app/group/controll/GroupController.java
@@ -3,6 +3,9 @@ package com.yedam.app.group.controll;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 @Controller
 public class GroupController {
 	
