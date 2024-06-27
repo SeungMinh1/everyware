00a677c4296abb3d2ae68fbacbf855a2bda3ade9
sanggomtaeng <huni980614@naver.com>
2024-06-27 18:03:54 +0900

    240627 KSH

--- everyware/src/main/java/com/yedam/app/approval/web/DraftController.java
+++ everyware/src/main/java/com/yedam/app/approval/web/DraftController.java
@@ -2,13 +2,15 @@ package com.yedam.app.approval.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yedam.app.approval.service.DraftService;
+import com.yedam.app.approval.service.DraftVO;
 
 @Controller
 public class DraftController {
 	@Autowired
 	DraftService draftService;
-	
 	
 }
