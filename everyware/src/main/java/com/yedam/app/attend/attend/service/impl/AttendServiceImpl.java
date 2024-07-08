1292e3a8d8c3b6c6c9abd85be11b3ecc1c447fa9
admin <admin@YD05-09>
2024-07-08 19:13:58 +0900

    2024/07/08

--- everyware/src/main/java/com/yedam/app/attend/attend/service/impl/AttendServiceImpl.java
+++ everyware/src/main/java/com/yedam/app/attend/attend/service/impl/AttendServiceImpl.java
@@ -70,18 +70,6 @@ public class AttendServiceImpl implements AttendService {
 		result.setExtrawork1(totalExtraWork);
 		result.setTotalwork1(totalWork);
 		
-		//지난주
-//		week =7;
-//		totalWork = 0;
-//		totalExtraWork = 0;
-//		List<AttendVO> list2 = attendMapper.countWorkTime(attendVO, week); //저번주 근무기록
-//		for(int i=0; i< list2.size(); i++) {
-//			totalWork += list2.get(i).getWorkTime(); //저번주 누적 근무기록
-//			totalExtraWork += list2.get(i).getExceedWorkTime(); //저번주 누적 초과근무기록
-//		}
-//		result.setExtrawork2(totalExtraWork);
-//		result.setTotalwork2(totalWork);
-		
 		return result;
 	}
 	@Override
