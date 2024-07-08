024f6306258edbf48f25cc988df640370d394c99
Ejfq <thdwogus07@naver.com>
2024-07-08 17:48:49 +0900

    Merge branch 'master' of https://github.com/SeungMinh1/everyware.git into Ejfq

--- everyware/src/main/java/com/yedam/app/schedule/web/ScheduleController.java
+++ everyware/src/main/java/com/yedam/app/schedule/web/ScheduleController.java
@@ -75,17 +75,45 @@ public class ScheduleController {
 	//근무조 설정
 	@PostMapping("/setSquEmp")
 	@ResponseBody
-	public String squadSetProcess() {
+	public String squadSetProcess(@RequestBody ScheduleVO scheduleVO) {
 		//근무조 설정 전 db 비우기
 		scheduleService.deleteAllsquad();
 		//근무조 설정
-		
+		scheduleService.insertSquad(scheduleVO);
 		
 		return "1";
 	}
 	
+	//근무표 생성 전 확인 화면
+	@GetMapping("/squadInfo")
+	public String squadInfo(Model model) {
+		//근무표 설정 가져오기
+		ScheduleVO scheduleVO = scheduleService.selectSquadInfo();
+		model.addAttribute("squadInfo", scheduleVO);
+		
+		//근무조 편성 가져오기
+		int squadNum = scheduleVO.getSquadNo();
+		List<ScheduleVO> list = scheduleService.selectSquadEmp();
+		model.addAttribute("squadNum", squadNum);
+		model.addAttribute("squadEmp", list);
+		
+		
+		return "schedule/squadInfo";
+	}
 	
-	
+	//근무표 생성
+	@GetMapping("/makeSchedule")
+	public String makeSchedule(Model model) {
+		//근무표 설정 가져오기
+		ScheduleVO scheduleVO = scheduleService.selectSquadInfo();
+		model.addAttribute("squadInfo", scheduleVO);
+		
+		//근무조 몇조인지
+		int squadNum = scheduleVO.getSquadNo();
+		model.addAttribute("squadNum", squadNum);
+		
+		return"schedule/makeSchedule";
+	}
 	
 	
 	
