75797e8fb62af72ab058095f37b27e9cb2cdcdb5
Ejfq <thdwogus07@naver.com>
2024-07-09 17:55:05 +0900

    0709

--- everyware/src/main/resources/static/js/schedule/makeSchedule.js
+++ everyware/src/main/resources/static/js/schedule/makeSchedule.js
@@ -7,6 +7,7 @@ let year = parseInt($("#calYear").attr("data-year"));
 let startDate = $("#scheduleCal").attr("data-startDate");
 let rotateCircle = $("#scheduleCal").attr("data-rotateCircle");// 교대주기
 let rotateNum = $("#scheduleCal").attr("data-rotateNum"); //하루교대수
+let squadNum = parseInt($("#calTbody").attr("data-squadNum")); //총 조 수
 
 
 
@@ -70,7 +71,7 @@ let rotateNum = $("#scheduleCal").attr("data-rotateNum"); //하루교대수
 	
 	
 	//몇조인지
-	let squadNum = $("#calTbody").attr("data-squadNum");
+	 squadNum = parseInt($("#calTbody").attr("data-squadNum"));
 	
 	//테이블 thead
 	 for(let i = 1 ; i <=date.getDate()  ; i ++){
@@ -93,33 +94,121 @@ let rotateNum = $("#scheduleCal").attr("data-rotateNum"); //하루교대수
 		html += "<th>"+ i + "조</th>"
 		for(let j = 1 ; j <= date.getDate() ; j++){
 			var zeroDay = ("00"+(j).toString()).slice(-2)
-			html += "<td data-date=" + year+ "-"+ zeroMonth+"-"+zeroDay + " data-squadNum = " + i + "></td>"
+			html += "<td data-date=" + year+ "-"+ zeroMonth+"-"+zeroDay + " data-squNum = " + i + "></td>"
 		}
 		html += "</tr>"
 		
 		$("#calTbody").append(html)
 	}
- 
+ 	
+	let standard = new Date(startDate);
  	//근무표시
- 	for(let i = 1 ; i <= date.getDate(); i++){
-		$("#calTbody").find('td:eq('+ i +')').css("backgroud-color" , 'red')
+ 	/*if((((new Date($("#calTable").find("td:eq(9)").attr("data-date")) - standard)/(1000 * 60 * 60 * 24))%squadNum )-$("#calTable").find("td:eq(9)").attr("data-squadNum") == -1){
+		
+		console.log(((new Date($("#calTable").find("td:eq(9)").attr("data-date")) - standard)));
 	}
  	
+ 	if($("#calTable").find("td").attr("data-squadNum") == 1){
+		$(this).attr("class", "rotate1")
+	}*/
+	
+	
+	for(let i = 0 ; i < date.getDate()*squadNum ; i++){
+		let dataDate = $("#calTable").find("td:eq("+ i +")").attr("data-date");
+		let dataSquadNum =  $("#calTable").find("td:eq("+ i +")").attr("data-squNum")
+		let diff = Math.floor(((new Date(dataDate) - standard)/ (1000 * 60 * 60 * 24))/rotateCircle);
+		let result = (diff % squadNum) - dataSquadNum
+		if(diff >= 0){	
+			if(rotateNum == 2){
+				if(result == -1){
+					$("#calTable").find("td:eq("+ i +")").attr("class", "rotate1")
+					let rotate1Squad = parseInt($("#calTable").find("td:eq("+ i +")").attr("data-squNum"));
+					
+					let rotate2Squad = (rotate1Squad - 1) <= 0 ?  ((rotate1Squad - 1) + squadNum) : (rotate1Squad - 1)
+					console.log(rotate2Squad)
+					$("#calTable").find("[data-squNum="+rotate2Squad+"][data-date="+ dataDate+"]").attr("class", "rotate2")
+					
+				}
+		
+				$(".rotate1").text("주간")
+				$(".rotate2").text("야간")
+				
+			}else if(rotateNum == 3){
+				if(result == -1){
+					$("#calTable").find("td:eq("+ i +")").attr("class", "rotate1")
+					let rotate1Squad = parseInt($("#calTable").find("td:eq("+ i +")").attr("data-squNum"));
+					
+					let rotate2Squad = (rotate1Squad - 1) <= 0 ?  (rotate1Squad - 1) +squadNum : (rotate1Squad - 1)
+					$("#calTable").find("[data-squNum="+rotate2Squad+"][data-date="+ dataDate+"]").attr("class", "rotate2")
+					
+					let rotate3Squad = (rotate1Squad - 2) <= 0 ?  (rotate1Squad - 2)+ squadNum : (rotate1Squad - 2)
+					$("#calTable").find("[data-squNum="+rotate3Squad+"][data-date="+ dataDate+"]").attr("class", "rotate3")
+				}
+				
+				$(".rotate1").text("오전")
+				$(".rotate2").text("오후")
+				$(".rotate3").text("야간")
+			}
+		}
+			
+	}
  	
- 	clickEvent();
+ 	clickEvent();	
  
  } //makeTable end
  	
  	//테이블 td클릭 이벤트
  	function clickEvent(){
 		$("#calTable").find("td").click(function(e){
-		let date = new Date($(e.target).attr("data-date"));
-		let standard = new Date(startDate);
+		date = new Date($(e.target).attr("data-date"));
+		standard = new Date(startDate);
 		
 		console.log((date - standard) / (1000 * 60 * 60 * 24));
 		
+		
 	})
 	}
+	
+	$(document).ready(function(){
+		$('#savePdf').click(function() { // pdf저장 button id
+		
+	    html2canvas($('#calTable')[0]).then(function(canvas) { //저장 영역 div id
+					
+		
+	    // 캔버스를 이미지로 변환
+	    var imgData = canvas.toDataURL('image/png');
+	    
+		     
+	    var imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
+	    var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+	    var imgHeight = canvas.height * imgWidth / canvas.width;
+	    var heightLeft = imgHeight;
+	    var rotation = -Math.PI * 0.5;
+	    var margin = 10; // 출력 페이지 여백설정
+	    var doc = new jsPDF('p', 'mm');
+	    var position = 0;
+	       
+	    // 첫 페이지 출력
+	    doc.addImage(imgData, 'PNG', rotation ,margin, position,  imgWidth, imgHeight);
+	    
+	    heightLeft -= pageHeight;
+	         
+	    // 한 페이지 이상일 경우 루프 돌면서 출력
+	    while (heightLeft >= 20) {
+	        position = heightLeft - imgHeight;
+	        doc.addPage();
+	        doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+	        heightLeft -= pageHeight;
+	    }
+	 
+	    // 파일 저장
+	    doc.save('file-name.pdf');
+
+		  
+		});
+		
+		});
+	})
  	
  
  
