3054ed957555b2d893374bb39bda92e2577055cb
KIMZMIN <seeaajelly@gmail.com>
2024-07-15 00:32:09 +0900

    240715

--- everyware/src/main/java/com/yedam/app/group/controll/GroupController.java
+++ everyware/src/main/java/com/yedam/app/group/controll/GroupController.java
@@ -33,9 +33,9 @@ public class GroupController {
 		mailVO.setMailboxId("d1");
 		
 		int empId = AuthUtil.getEmpId();
-		List<MailVO> find = mailService.mailboxInfo(mailVO, empId);
+		//List<MailVO> find = mailService.mailboxInfo(mailVO, empId);
 		
-		model.addAttribute("mailboxInfo", find);
+		//model.addAttribute("mailboxInfo", find);
 
 		
 		return "main/index";
