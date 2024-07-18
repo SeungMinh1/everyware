85ee5f4269949d9f3a581430ea732f6a42ea90ef
sanggomtaeng <huni980614@naver.com>
2024-07-18 15:52:43 +0900

    240718 KSH

--- everyware/src/main/java/com/yedam/app/approval/mapper/DraftMapper.java
+++ everyware/src/main/java/com/yedam/app/approval/mapper/DraftMapper.java
@@ -1,6 +1,9 @@
 package com.yedam.app.approval.mapper;
 
+import java.util.List;
+
 import com.yedam.app.approval.service.DraftVO;
+import com.yedam.app.approval.service.TaskVO;
 
 public interface DraftMapper {
 		// 기안
@@ -8,4 +11,7 @@ public interface DraftMapper {
 		
 		// 기안문서조회
 		public DraftVO draftInfo(DraftVO draftVO);
+		
+		// 문서 양식
+		public List<TaskVO> taskList();
 }
--- everyware/src/main/java/com/yedam/app/approval/service/DraftService.java
+++ everyware/src/main/java/com/yedam/app/approval/service/DraftService.java
@@ -1,9 +1,14 @@
 package com.yedam.app.approval.service;
 
+import java.util.List;
+
 public interface DraftService {
 	// 기안
 	public int draftInsert(DraftVO draftVO);
 
 	// 기안문서조회
 	public DraftVO draftInfo(DraftVO draftVO);
+	
+	// 문서 양식
+	public List<TaskVO> taskList();
 }
